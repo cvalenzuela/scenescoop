cd2f86853f55e4fdd4c18338157878fb83d54fe9
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-11-05 15:42:41 -0500

    add credentials

---
+++ credentials.py
@@ -0,0 +1,94 @@
+account_sid = ""
+auth_token = ""
+number = ""
+
+movies = [
+  {
+    "name": "Gone with the Wind (1939) - Victor Fleming",
+    "file": "GoneWithTheWind.mkv"
+  },
+  {
+    "name": "Rebel Without a Cause (1955) - Nicholas Ray",
+    "file": "RebelWithoutACause.mp4"
+  },
+  {
+    "name": "Vertigo (1958) - Alfred Hitchcock",
+    "file": "Vertigo.mp4",
+  },
+  {
+    "name": "F for Fake (1973) - Orson Welles",
+    "file": "FForFake.mp4"
+  },
+  {
+    "name": "Star Wars: A New Hope (1977) - George Lucas",
+    "file": "ANewHope.mkv"
+  },
+  {
+    "name": "Paris, Texas (1984) - Wim Wenders",
+    "file": "ParisTexas.mkv"
+  },
+  {
+    "name": "Rosemary's Baby (1993) - Roman Polanski",
+    "file": "RosemarysBaby.mp4",
+  },
+  {
+    "name": "Dazed and Confused (1993) - Richard Linklater",
+    "file": "DazedAndConfused.mp4",
+  },
+  {
+    "name": "Ghost in the Shell (1995) - Mamoru Oshii",
+    "file": "GhostintheShell.mp4"
+  },
+  {
+    "name": "Eyes Wide Shut (1999) - Stanley Kubrick",
+    "file": "EyesWideShut.mkv"
+  },
+  {
+    "name": "Le Fils (2002) - Jean-Pierre and Luc Dardenne",
+    "file": "LeFils.avi",
+  },
+  {
+    "name": "Children of Men (2006) - Alfonso Cuarón",
+    "file": "ChildrenofMen.mp4"
+  },
+  {
+    "name": "Black Mirror (2011) - Episode 3",
+    "file": "BlackMirror.avi",
+  },
+  {
+    "name": "Drive (2011) - Nicolas Refn",
+    "file": "Drive.mp4"
+  },
+  {
+    "name": "Moonrise Kingdom (2012) - Wes Anderson",
+    "file": "MoonriseKingdom.mp4",
+  },
+  {
+    "name": "Samsara (2012) - Ron Fricke",
+    "file": "Samsara.avi",
+  },
+  {
+    "name": "Her (2013) - Spike Jonze",
+    "file": "Her.mkv",
+  },
+  {
+    "name": "Fargo: Season 1, Episode 1 (2014) - Noah Hawley",
+    "file": "FargoS01E01.mkv"
+  },
+  {
+    "name": "Fantastic Beasts and Where to Find Them (2016) - David Yates",
+    "file": "FantasticBeastsandWheretoFindThem.mp4"
+  },
+  {
+    "name": "Café Society (2016) - Woody Allen",
+    "file": "CafeSociety.mkv"
+  },
+  {
+    "name": "Stranger Things: Season 1, Episode 1 (2016) - The Duffer Brothers",
+    "file": "StrangerThingsE1.mp4"
+  },
+  {
+    "name": "American Made (2017) - Doug Liman",
+    "file": "AmericanMade.mkv"
+  }
+]
